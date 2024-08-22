db56f692c3d243908dc9fcb0b95eba2e2d921d8f
Anurag Kaushik <anurag11b12345@gmail.com>
2024-08-22 10:44:36 +0530

    Create SquarePerimeter.java

---
+++ SquarePerimeter.java
@@ -0,0 +1,8 @@
+public class SquarePerimeter {
+    public static void main(String[] args) {
+        double side = 6.0; // side length of the square
+        double perimeter = 4 * side; // calculate perimeter using formula Perimeter = 4 × side
+
+        System.out.println("Perimeter of the square: " + perimeter + " units");
+    }
+}
